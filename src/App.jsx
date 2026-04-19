5a38318be2cdb65eb455cd8f582023258ab1d28f
Anna Kompan <annakompanch@gmail.com>
2026-04-19 21:18:17 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+function App() {
+  return (
+    <>
+      <Header />
+      <Food></Food>
+      <Food></Food>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
